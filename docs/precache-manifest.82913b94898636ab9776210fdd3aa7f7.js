d7ae9c7f20cbc9c6fa0b860ebc6ae6e5d75ad383
Alejandro Diez <adilovi95@gmail.com>
2020-09-02 19:04:43 +0200

    Agregado

---
+++ docs/precache-manifest.82913b94898636ab9776210fdd3aa7f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f285188d578656f2bcc1d36ef2d1d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f4f21d387951d675147",
+    "url": "/static/css/main.7b9d3f51.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4f21d387951d675147",
+    "url": "/static/js/main.767e2750.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
